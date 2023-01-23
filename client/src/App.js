2141f876a4a8fff2e7bdb951dcf82927cb728f46
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2023-01-23 21:57:18 +0100

    commit api

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RevenueByProductLine from './RevenueByProductLine';
+function App() {
+  return (
+    <div>
+    <h2>Revenue by Product Line</h2>
+    <RevenueByProductLine />
+  </div>
+  );
+}
+
+export default App;
