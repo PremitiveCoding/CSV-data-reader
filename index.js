020510f3a06c244e56b6976193293999d9f70791
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2023-01-25 04:02:22 +0100

    ok

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const Sale = require('./Models/Sale');
-const moment = require('moment');
 
 
 // Mongo connection
